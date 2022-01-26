3ee77decd9cfc6b94ccfde6d173e724f0737fad5
Mathew Dunne <mathew.dunne@cntrline.com>
2022-01-26 15:43:16 -0500

    Added click physics to rotate gem

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import * as THREE from 'https://cdn.skypack.dev/three'
-import {OrbitControls} from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js'
+import * as CANNON from '../node_modules/cannon/cannon-es.js'
+import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
@@ -14,14 +15,27 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 renderer.render(scene, camera)
 
-const backgroundTexture = new THREE.TextureLoader().load('../assets/space.jpg')
+const backgroundTexture = new THREE.TextureLoader().load('../assets/img/space.jpg')
 scene.background = backgroundTexture
+
+const world = new CANNON.World()
+
+let screenMidx = window.innerWidth/2
+let screenMidy = window.innerHeight/2
 
 // set up shape
 const icoGeometry = new THREE.IcosahedronGeometry(20, 0)
 const icoMaterial = new THREE.MeshStandardMaterial({color: 0x46d4a2})
 const ico = new THREE.Mesh(icoGeometry, icoMaterial)
+const icoBodyShape = new CANNON.Box(new CANNON.Vec3(1, 1, 1))
+var icoBody = new CANNON.Body({
+  mass: 1,
+})
+icoBody.addShape(icoBodyShape)
+// icoBody.angularVelocity.set(10, 10, 10)
+icoBody.angularDamping = 0.3
 scene.add(ico)
+world.addBody(icoBody)
 
 // set up point lighting
 const pointLightArray = Array()
@@ -55,8 +69,9 @@ const ambientLight = new THREE.AmbientLight(0xfffff, 0.4)
 scene.add(ambientLight)
 
 // orbit controls
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableZoom = false;
+const orbitControls = new OrbitControls(camera, renderer.domElement)
+orbitControls.enableZoom = false;
+
 
 // add "stars"
 function addStar(geometry, material) {
@@ -73,22 +88,37 @@ for (let i=0; i<200; i++) {
   addStar(starGeometry, starMaterial)
 }
 
-// move camera down when scrolling
-//document.body.onscroll = moveCamera
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top
-  camera.position.z = 30 + t*-0.1
-  //update shape and light positions
-  ico.position.z = t*-0.1
-  for (let i=0; i < pointLightArray.length; i++) {
-    var offset = (i%2 > 0 == 0 ? positionFactor : -1*positionFactor)
-    pointLightArray[i].position.z = offset + t*-0.1
-  }
+function orbitCamera() {
+  camera.position.x += 0.07*Math.sin(a)
+  camera.position.z += 0.07*Math.cos(a)
+  camera.lookAt([0, 0, 0])
 }
 
+// when window is resized
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  screenMidx = window.innerWidth/2
+  screenMidy = window.innerHeight/2
+}
+window.addEventListener('resize', onWindowResize)
 
+// on click
+function printMousePos(event) {
+  console.log("clientX: " + event.clientX + " - clientY: " + event.clientY)
+  const torqueFactor = 100
+  let yVec = screenMidx - event.clientX
+  let zVec = screenMidy - event.clientY
+  const mag = Math.sqrt(yVec*yVec + zVec*zVec)
+  yVec /= mag
+  zVec/= mag
 
+  const torque = new CANNON.Vec3(0, yVec*torqueFactor, zVec*torqueFactor)
+  icoBody.applyTorque(torque)
+}
+
+document.addEventListener("click", printMousePos);
 
 // main animation loop
 var a = 0
@@ -96,13 +126,7 @@ function animate() {
   requestAnimationFrame(animate)
 
   //animations go here
-  ico.rotation.x += 0.003
-  ico.rotation.y += 0.001
-  ico.rotation.z += 0.005
-
-  camera.position.x += 0.07*Math.sin(a)
-  camera.position.z += 0.07*Math.cos(a)
-  camera.lookAt([0, 0, 0])
+  orbitCamera()
 
   if (a==360) {
     a = 0
@@ -112,7 +136,13 @@ function animate() {
   
   
   //update controls
-  controls.update()
+  orbitControls.update()
+
+  //update physics
+  world.fixedStep()
+  // Copy coordinates from cannon.js to three.js
+  ico.position.copy(icoBody.position)
+  ico.quaternion.copy(icoBody.quaternion)
 
 
   renderer.render(scene, camera)
